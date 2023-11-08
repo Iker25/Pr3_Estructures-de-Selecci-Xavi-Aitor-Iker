d56ce517cb1fce753efbaab0774db37ab9d9f523
Ikerjl25 <iker.jimenez.7e5@itb.cat>
2023-11-08 12:21:12 +0100

    Pujar arxius

--- e1.py
+++ e1.py
@@ -1,1 +1,16 @@
+"""
+Iker Jiménez López, Aitor Palma, Xavier Cabello
+8/11/2023
+e1 "Is bigger"
+"""
 
+# Pide dos numeros
+num1, num2 = map(float, input("Añade dos números separados por un espacio: ").split())
+
+# Garantiza que el numero1 sea mas pequeño que el numero2
+if num1 > num2:
+    num1, num2 = num2, num1
+
+# Muestra los valores por pantalla
+print("El primer número és:", num1)
+print("El segundo número és:", num2)
