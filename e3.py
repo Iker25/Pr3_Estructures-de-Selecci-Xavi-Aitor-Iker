d576228c68bfd8dd624a7a39a2adcd2637223cae
Ikerjl25 <iker.jimenez.7e5@itb.cat>
2023-11-08 12:48:43 +0100

    Pujar arxius

--- e3.py
+++ e3.py
@@ -1,1 +1,20 @@
+"""
+Iker Jiménez López, Aitor Palma, Xavier Cabello
+8/11/2023
+e3 "fastAndFurios"
+"""
 
+
+# Solicita la velocidad inicial, la aceleración y el tiempo
+v_i = float(input("Cual es velocidad inicial? "))
+aceleracion = float(input("Cual es la aceleración? "))
+tiempo = float(input("Cual es el tiempo? "))
+
+# Comprueba si el objeto está parado
+if (v_i + (aceleracion * tiempo)) <= 0:
+    print("El objeto está parado, por lo tanto no se puede calcular la velocidad media.")
+else:
+    # Calcula la velocidad media
+    v_m = v_i + (aceleracion * tiempo) / 2
+    print("La velocidad instantánea es:", v_i + (aceleracion * tiempo), "m/s")
+    print("La velocidad media es:", v_m, "m/s")
